d74d7915c313cec46dcaeb10a162eb0fc440685e
Darren Teo <154348861+darHH@users.noreply.github.com>
2026-06-11 23:07:51 +0800

    Add: find-target-in-rotated-sorted-array - submission-4

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-4.py
@@ -0,0 +1,29 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+
+        def bin_search_on_sorted(lp: int, rp: int) -> int:
+            while lp <= rp:
+                mp = (rp - lp) // 2 + lp
+                if nums[mp] == target:
+                    return mp
+                elif nums[mp] < target:
+                    lp = mp + 1
+                else:
+                    rp = mp - 1
+            return -1
+        
+        #find the two sorted segments
+        lp, rp = 0, len(nums) - 1
+        while lp < rp:
+            mp = (rp - lp) // 2 + lp
+            if nums[mp] > nums[rp]:
+                lp = mp + 1
+            else:
+                rp = mp
+        #nums[lp] contains min value
+        # print(lp)
+
+        return max(bin_search_on_sorted(lp, len(nums) - 1), bin_search_on_sorted(0, lp - 1))
+
+
+    
