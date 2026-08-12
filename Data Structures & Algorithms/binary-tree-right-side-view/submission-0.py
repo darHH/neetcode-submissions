6b04b645573150fcb240804d7861860d11d96d4d
Darren Teo <154348861+darHH@users.noreply.github.com>
2026-08-12 23:56:22 +0800

    Add: binary-tree-right-side-view - submission-0

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-0.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        helper_answer = []
+
+        if root:
+            # in temp_deque, i will store arrays with 2 elements -> first is the node, second is the node's level
+            temp_deque = deque()
+            temp_deque.append([root, 0])
+
+            while temp_deque:
+                curr_node, curr_level = temp_deque.popleft()
+
+                if curr_node.left:
+                    temp_deque.append([curr_node.left, curr_level + 1])
+                if curr_node.right:
+                    temp_deque.append([curr_node.right, curr_level + 1])
+                
+                if curr_level == len(helper_answer):
+                    helper_answer.append([])
+                helper_answer[curr_level].append(curr_node.val)
+
+        # print(helper_answer)
+        # helper_answer is the previous question's solution (i.e., level order traversal of it as a nested list, 
+        # where each sublist contains the values of nodes at a particular level in the tree, from left to right.
+        # to get the answer for right side view , i just take the rightmost value of the helper_answer
+        answer = []
+        for list in helper_answer:
+            answer.append(list[-1])
+        return answer
